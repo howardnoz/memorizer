08051a6003cbd00aa8a46446ffc7a998e3617a59
Howard Noz <howardcnoz@gmail.com>
2017-03-16 06:45:36 +0000

    Customize home page.

---
+++ resources/views/exercise.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Memorizer</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="{{ url('/css/home.css') }}" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+
+                <div class="links">
+                    <a href="#">Home</a>
+                    <a href="exercise">Exercise</a>
+                </div>
+
+            <div class="content">
+                <div class="title m-b-md">
+			May force be with you...
+                </div>
+
+		<br />
+		<br />
+		<form>
+			<input type="text" />
+			<input type="submit" />
+		</form>
+
+            </div>
+        </div>
+    </body>
+</html>
